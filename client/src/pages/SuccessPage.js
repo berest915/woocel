45a6ed585c057987b89eb83215e89dc17105887b
berest915 <berest915@gmail.com>
2020-11-04 02:51:00 +0800

    testo version saved - attempt to restructure styling hierachy

--- client/src/pages/SuccessPage.js
+++ client/src/pages/SuccessPage.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Success from "../coms/Success/Success";
+
 
 const SuccessPage = () => {
   return (
     <>
      
-        <Success />
-    
     </>
   );
 };
