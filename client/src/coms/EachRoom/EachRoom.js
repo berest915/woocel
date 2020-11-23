a6682cec5578f95375112684058f303cc6f3858f
berest915 <berest915@gmail.com>
2020-11-24 01:46:56 +0800

    fix 1-digit day or month output

--- client/src/coms/EachRoom/EachRoom.js
+++ client/src/coms/EachRoom/EachRoom.js
@@ -46,10 +46,18 @@ const EachRoom = ({
       const dateObj = new Date(dateNum)
      
 
-      const timestamp = new Date(messages[0].timestamp?.toDate());
+      // const timestamp = new Date(messages[0].timestamp?.toDate());
       const year = dateObj.getFullYear();
-      const month = dateObj.getMonth() + 1;
-      const day = dateObj.getDate();
+  
+      const month =
+        dateObj.getMonth() >= 10
+          ? dateObj.getMonth()
+          : `0${dateObj.getMonth() + 1}`;
+    
+      const day =
+        dateObj.getDate() >= 10
+          ? dateObj.getDate()
+          : `0${dateObj.getDate()}`;
       setLastDate(day + "/" + month + "/" + year);
     }
   }, [messages]);
