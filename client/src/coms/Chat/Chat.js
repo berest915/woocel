b7c32134b0102a2ad94798679aed4ab51a12350e
berest915 <berest915@gmail.com>
2020-11-24 05:07:56 +0800

    fix messages.timestamp always overwritten by latest timestamp, attempt to revise this commit

--- client/src/coms/Chat/Chat.js
+++ client/src/coms/Chat/Chat.js
@@ -32,8 +32,6 @@ const Chat = () => {
   const onOpenModal = () => setIsOpen(true);
   const onCloseModal = () => setIsOpen(false);
 
-  const [lastHourMinute, setLastHourMinute] = useState("");
-
   let value = false;
 
   useEffect(() => {
@@ -77,32 +75,6 @@ const Chat = () => {
     }
   });
 
-  useEffect(() => {
-    if (messages.length > 0) {
-      getLastHourMinute();
-    }
-    function getLastHourMinute() {
-      const timestamp = new Date(
-        messages[messages.length - 1].timestamp?.toDate()
-      );
-      const year = timestamp.getFullYear();
-      const month = timestamp.getMonth() + 1;
-      const day = timestamp.getDate();
-      const hour =
-        timestamp.getHours() >= 10
-          ? timestamp.getHours()
-          : `0${timestamp.getHours()}`;
-      const minute =
-        timestamp.getMinutes() >= 10
-          ? timestamp.getMinutes()
-          : `0${timestamp.getMinutes()}`;
-
-      setLastHourMinute(
-        hour + ":" + minute + " " + day + "/" + month + "/" + year
-      );
-    }
-  }, [messages]);
-
   // https://codesandbox.io/s/chat-application-3t380
   const sendMessage = e => {
     e.preventDefault();
@@ -110,16 +82,43 @@ const Chat = () => {
   };
 
   useEffect(() => {
-    input &&
+    if (input) {
+      if (messages.length > 0) {
+        let prevTimestamp = messages[messages.length - 1].timestamp?.toDate();
+        console.log(prevTimestamp);
+      }
+
       db.collection("rooms").doc(roomId).collection("messages").add({
         message: input,
         name: user.displayName,
         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        formattedTimestamp: null,
+        isNewerDate: false,
       });
 
-    inputRef.current.value = "";
-    setInput(""); // reset input value
+      inputRef.current.value = "";
+      setInput(""); // reset input value
+    }
   }, [input, roomId, user.displayName]);
+
+  const fnFormatTimestamp = timestamp => {
+    const year = timestamp.getFullYear();
+    const month = timestamp.getMonth() + 1;
+    const day = timestamp.getDate();
+    const hour =
+      timestamp.getHours() >= 10
+        ? timestamp.getHours()
+        : `0${timestamp.getHours()}`;
+    const minute =
+      timestamp.getMinutes() >= 10
+        ? timestamp.getMinutes()
+        : `0${timestamp.getMinutes()}`;
+    const second = timestamp.getSeconds();
+
+    const formattedTimestamp = `${hour}:${minute}:${second} ${day}/${month}/${year}`;
+    
+    return formattedTimestamp;
+  };
 
   return (
     <>
@@ -165,24 +164,32 @@ const Chat = () => {
 
       {/*//! CHAT BODY  */}
       <div className="chat__body">
-        {messages.map((message, i) => (
-          <div key={i} ref={messageRef}>
-            {/* shud use sort of user id instead of name, in case exactly same username */}
-            <div
-              className={`chat__message ${
-                message.name === user.displayName && `chat__receiver`
-              }`}
-            >
-              <p className="chat__name">{message.name}</p>
-              <p className="chat__text">{message.message}</p>
+        {messages.map((message, i) => {
+          let timestamp = new Date(message.timestamp?.toDate());
+          const formattedTimestamp = fnFormatTimestamp(timestamp);
+          
+          return (
+            <div key={i} ref={messageRef}>
+              {/* shud use sort of user id instead of name, in case exactly same username */}
+              <div
+                className={`chat__newerDate ${true && `centeredNewerDate`}`}
+              ></div>
+              <div
+                className={`chat__message ${
+                  message.name === user.displayName && `chat__receiver`
+                }`}
+              >
+                <p className="chat__name">{message.name}</p>
+                <p className="chat__text">{message.message}</p>
 
-              <span className="chat__timestamp">
-                {/* {new Date(message.timestamp?.toDate()).toString()} */}
-                {lastHourMinute}
-              </span>
+                <span className="chat__timestamp">
+                  {/* {new Date(message.timestamp?.toDate()).toString()} */}
+                  {formattedTimestamp}
+                </span>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
 
       {/*//! CHAT FOOTER  */}
@@ -196,7 +203,6 @@ const Chat = () => {
             placeholder="Enter a message"
           />
           <button onClick={sendMessage}>Send a message</button>
-          {input}
         </form>
         <MicIcon />
       </div>
