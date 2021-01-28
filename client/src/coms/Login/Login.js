4c80540dc57ece652ef25f40d65dd9dba79cddae
berest915 <berest915@gmail.com>
2021-01-28 16:34:54 +0800

    revert env

--- client/src/coms/Login/Login.js
+++ client/src/coms/Login/Login.js
@@ -91,7 +91,7 @@ const Login = () => {
           placement={!isMd ? "right-end" : "bottom"}
         >
           <a
-            href={process.env.REACT_APP_DEMO_LINK}
+            href="https://youtu.be/lkqqRDXrpTk"
             alt="demo video link"
             target="_blank"
             rel="noreferrer"
